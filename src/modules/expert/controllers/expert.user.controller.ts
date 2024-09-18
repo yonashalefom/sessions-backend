5a26c5828abbe8688f432abc31795bdc23b2c782
Yonas Halefom <yonet1553@gmail.com>
2024-09-18 17:12:40 +0300

    feat: get expertise by groupping by expertise category feature added

--- src/modules/expert/controllers/expert.user.controller.ts
+++ src/modules/expert/controllers/expert.user.controller.ts
@@ -22,17 +22,21 @@ import {
     ENUM_POLICY_SUBJECT,
 } from 'src/modules/policy/enums/policy.enum';
 import { USER_DEFAULT_STATUS } from 'src/modules/user/constants/user.list.constant';
+import { ExpertsListByCategoryResponseDto } from 'src/modules/user/dtos/response/experts.list.by.category.response.dto';
 import { UserListResponseDto } from 'src/modules/user/dtos/response/user.list.response.dto';
 import { ENUM_ACTIVE_USER_STATUS } from 'src/modules/user/enums/user.enum';
-import { IUserDoc } from 'src/modules/user/interfaces/user.interface';
+import {
+    IExpertsByCategoryDoc,
+    IUserDoc,
+} from 'src/modules/user/interfaces/user.interface';
 import { UserService } from 'src/modules/user/services/user.service';
 
 @ApiTags('modules.shared.expert')
 @Controller({
     version: '1',
-    path: '/',
+    path: '/experts',
 })
-export class ExpertController {
+export class ExpertUserController {
     constructor(
         private readonly userService: UserService,
         private readonly paginationService: PaginationService
@@ -49,7 +53,7 @@ export class ExpertController {
     @AuthJwtAccessProtected()
     @ApiKeyProtected()
     @Get('get/all')
-    async list(
+    async getAllExperts(
         @PaginationQuery({
             availableSearch: EXPERT_DEFAULT_AVAILABLE_SEARCH,
         })
@@ -85,7 +89,100 @@ export class ExpertController {
             _limit
         );
 
+        console.log(JSON.stringify(experts, null, 2));
+
         const mapped = await this.userService.mapList(experts);
+
+        return {
+            _pagination: { total, totalPage },
+            data: mapped,
+        };
+    }
+    // endregion
+
+    // region Get All Experts (Group By Category)
+    @UserExpertsListDoc()
+    @ResponsePaging('expert.getAllExpertsByCategory')
+    @PolicyAbilityProtected({
+        subject: ENUM_POLICY_SUBJECT.EXPERT,
+        action: [ENUM_POLICY_ACTION.READ],
+    })
+    @PolicyRoleProtected(ENUM_POLICY_ROLE_TYPE.USER)
+    @AuthJwtAccessProtected()
+    @ApiKeyProtected()
+    @Get('get/all/by-category')
+    async getExpertsGroupByCategory(): Promise<
+        IResponsePaging<ExpertsListByCategoryResponseDto>
+    > {
+        const pipeline = [
+            {
+                $match: {
+                    role: '16c4b7f3-0c6b-4a99-a560-3ee99c1b0730',
+                },
+            },
+            {
+                $unwind: {
+                    path: '$expertise',
+                    preserveNullAndEmptyArrays: true,
+                },
+            },
+            {
+                $lookup: {
+                    from: 'Categories',
+                    localField: 'expertise',
+                    foreignField: '_id',
+                    as: 'expertise',
+                },
+            },
+            {
+                $unwind: {
+                    path: '$expertise',
+                    preserveNullAndEmptyArrays: true,
+                },
+            },
+            {
+                $group: {
+                    _id: '$expertise._id',
+                    expertiseCategory: {
+                        $first: '$expertise.category',
+                    },
+                    expertiseDescription: {
+                        $first: '$expertise.description',
+                    },
+                    users: {
+                        $push: {
+                            _id: '$_id',
+                            name: '$name',
+                            username: '$username',
+                            email: '$email',
+                            address: '$address',
+                        },
+                    },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    expertiseCategoryId: '$_id',
+                    expertiseCategory: 1,
+                    expertiseDescription: 1,
+                    users: 1,
+                },
+            },
+        ];
+
+        const expertsByCategory: IExpertsByCategoryDoc[] =
+            await this.userService.groupByExpertise(pipeline);
+
+        const total: number =
+            await this.userService.getTotalAggregate(pipeline);
+        const totalPage: number = this.paginationService.totalPage(
+            total,
+            _limit
+        );
+
+        const mapped =
+            await this.userService.mapExpertsByCategoryList(expertsByCategory);
 
         return {
             _pagination: { total, totalPage },
--- src/router/routes/routes.expert.module.ts
+++ src/router/routes/routes.expert.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { CategoryModule } from 'src/modules/category/category.module';
-import { ExpertController } from 'src/modules/expert/controllers/expert.controller';
 import { ExpertModule } from 'src/modules/expert/expert.module';
 import { UserExpertController } from 'src/modules/user/controllers/user.expert.controller';
 import { UserModule } from 'src/modules/user/user.module';
 
 @Module({
-    controllers: [UserExpertController, ExpertController],
+    controllers: [UserExpertController],
     providers: [],
     exports: [],
     imports: [UserModule, ExpertModule, AuthModule, CategoryModule],
